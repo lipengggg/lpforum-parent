52285251824c70612bb532ef86cf2e1ff12515fd
李鹏 <lipeng@wanda.cn>
2019-04-25 10:29:09 +0800

    日志过滤器

---
+++ lpforum-gateway/src/main/java/com/ylsdd/lpforum/lpforumgateway/filter/LogFilter.java
@@ -0,0 +1,42 @@
+package com.ylsdd.lpforum.lpforumgateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ *
+ * @author lipeng
+ * @version Id: LogFilter.java, v 0.1 2019/4/25 10:27 lipeng Exp $$
+ */
+public class LogFilter implements GatewayFilter,Ordered{
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    @Override
+    public ShortcutType shortcutType() {
+        return null;
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return null;
+    }
+
+    @Override
+    public String shortcutFieldPrefix() {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+}
